2b7a0b4e55b5a8ced346141309d9d4ddace9f355
Melanie Reis <166030520+melaniereis@users.noreply.github.com>
2024-11-20 13:37:54 +0000

    add add_prime_sum exercise unfinished and correct first_word

--- Rank 2/Level1/first_word/fisrt_word.c
+++ Rank 2/Level1/first_word/fisrt_word.c
@@ -16,5 +16,4 @@ int main(int argc, char **argv)
 		}
     }
     write(1, "\n", 1);
-    return 0;
 }
---
+++ Rank 2/Level3/add_prime_sum/add_prime_sum.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    if (argc == 2)
+    {
+        if (argv[1] > 0)
+        {
+
+        }
+    }
+    write(1, "0\n", 2);
+}
+
+int ft_atoi(char *str)
+{
+    
+}
